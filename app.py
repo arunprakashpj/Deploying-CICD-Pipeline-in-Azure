d8b3ef25626a2692b594795d583284cc121dbbb8
arunprakashpj <arunprakashpj@gmail.com>
2021-01-24 09:28:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def predict():
 
     try:
         clf = joblib.load("boston_housing_prediction.joblib")
-    except Exception as e:
+    except:
         LOG.info("JSON payload: %s json_payload")
         return "Model not loaded"
 
