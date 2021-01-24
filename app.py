0d5cf7bcd9c4301fb7a74fd8ee13ea203f6e89b0
arunprakashpj <arunprakashpj@gmail.com>
2021-01-24 09:11:25 +0100

    Add Generic Exception type

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def predict():
 
     try:
         clf = joblib.load("boston_housing_prediction.joblib")
-    except:
+    except Exception as e:
         LOG.info("JSON payload: %s json_payload")
         return "Model not loaded"
 
