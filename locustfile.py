582c7f18a86416872e7845f9d870d20f68585113
arunprakashpj <arunprakashpj@gmail.com>
2021-01-29 13:30:44 +0100

    Update locustfile

--- locustfile.py
+++ locustfile.py
@@ -1,33 +1,29 @@
-import time
-from locust import  HttpUser, task, between
-
-class MyUser(HttpUser):
-  host = r'https://demowebapp.azurewebsites.net:443/predict'
-
-  @task
-  def prediction(self):
-    post_data = {
-        "CHAS":{
-            "0":0
-        },
-        "RM":{
-            "0":6.575
-        },
-        "TAX":{
-            "0":296.0
-        },
-        "PTRATIO":{
-            "0":15.3
-        },
-        "B":{
-            "0":396.9
-        },
-        "LSTAT":{
-            "0":4.98
-        }
-    }
-    
-    post_headers = {r'Content-Type': r'application/json'}
-
-
-    response = self.client.request(method='POST',path=self.host,json=post_data )
+import random
+from locust import HttpUser, task, between
+class QuickstartUser(HttpUser):
+    wait_time = between(5, 10)
+    @task
+    def index(self):
+        self.client.get("/")
+    @task(3)
+    def checkPredict(self):
+        self.client.post("/predict", json={
+            "CHAS":{
+                "0":0
+            },
+            "RM":{
+                "0":6.575
+            },
+            "TAX":{
+                "0":296.0
+            },
+            "PTRATIO":{
+                "0":15.3
+            },
+            "B":{
+                "0":396.9
+            },
+            "LSTAT":{
+                "0":4.98
+            }
+        })
